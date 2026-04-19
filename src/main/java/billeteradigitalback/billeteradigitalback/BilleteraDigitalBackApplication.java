44957cf6aebd6e03a484ac446fc93bebeba6dffc
J-Dav-A <159507313+J-Dav-A@users.noreply.github.com>
2026-04-18 20:53:08 -0500

    si

---
+++ src/main/java/billeteradigitalback/billeteradigitalback/BilleteraDigitalBackApplication.java
@@ -0,0 +1,13 @@
+package billeteradigitalback.billeteradigitalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilleteraDigitalBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilleteraDigitalBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/billeteradigitalback/billeteradigitalback/BilleteraDigitalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package billeteradigitalback.billeteradigitalback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilleteraDigitalBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
